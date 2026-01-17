cf179376909d1404b87a88d8fac7c89ff58b046c
yersonh <solanoalfonsoy@gmail.com>
2026-01-17 07:11:56 -0500

    second commit

--- public/referenciador.php
+++ public/referenciador.php
@@ -538,20 +538,17 @@ $barrios = $barrioModel->getAll();
 
     <!-- Footer -->
     <footer class="system-footer">
-        <!-- Logo centrado -->
-        <div class="container text-center mb-3">
-            <img src="imagenes/Logo-artguru.png" alt="Logo">
-        </div>
-        <div class="container">
-            <p>© Derechos de autor Reservados. 
-                <strong>Ing. Rubén Darío González García</strong> • 
-                SISGONTech • Colombia © • <?php echo date('Y'); ?>
-            </p>
-            <p>Contacto: <strong>+57 3106310227</strong> • 
-                Email: <strong>sisgonnet@gmail.com</strong>
-            </p>
-        </div>
-    </footer>
+            <div class="container text-center mb-3">
+                <img src="../imagenes/Logo-artguru.png" alt="Logo">
+            </div>
+
+            <div class="container text-center">
+                <p>
+                    © Derechos de autor Reservados • <strong>Ing. Rubén Darío González García</strong> • Equipo de soporte • SISGONTech<br>
+                    Email: sisgonnet@gmail.com • Contacto: +57 3106310227 • Puerto Gaitán, Colombia • <?php echo date('Y'); ?>
+                </p>
+            </div>
+        </footer>
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
